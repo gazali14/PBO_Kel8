525cbaa2df0497a3a775069eb90b8999b9a85f0b
gazali14 <gazali.1494@gmail.com>
2024-04-26 10:53:54 +0700

    initial Commit

---
+++ src/kuisionerudpk/KuisionerUDPK.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kuisionerudpk;
+
+/**
+ *
+ * @author U53R
+ */
+public class KuisionerUDPK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //commit awal
+    }
+    
+}
