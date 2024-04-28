b747ad8e14bbaa948f964b46052dae73b99ef68c
gazali14 <gazali.1494@gmail.com>
2024-04-28 22:36:15 +0700

    commit's gazali

--- src/kuisionerudpk/QuestionaireData.java
+++ src/kuisionerudpk/QuestionaireData.java
@@ -9,5 +9,135 @@ package kuisionerudpk;
  * @author U53R
  */
 public class QuestionaireData {
-    
+    private Perusahaan perusahaan;
+    private KIP kip;
+    private String statusUsaha;
+    private String gred;
+    private String badanUsaha;
+    private String pekerjaanUtama;
+    private String bidangPekerjaanUtama;
+    private String tempatUsaha;
+    private int banyakPekerjaTetap;
+    private ContactPerson contactPerson;
+    private Pencacah pencacah;
+    private Pengawas pengawas;
+    private String catatan;
+    private Validator validasi;
+
+    public QuestionaireData() {
+    }
+
+    // Getter dan setter untuk semua atribut
+    public Perusahaan getPerusahaan() {
+        return perusahaan;
+    }
+
+    public void setPerusahaan(Perusahaan perusahaan) {
+        this.perusahaan = perusahaan;
+    }
+
+    public KIP getKip() {
+        return kip;
+    }
+
+    public void setKip(KIP kip) {
+        this.kip = kip;
+    }
+
+    public String getStatusUsaha() {
+        return statusUsaha;
+    }
+
+    public void setStatusUsaha(String statusUsaha) {
+        this.statusUsaha = statusUsaha;
+    }
+
+    public String getGred() {
+        return gred;
+    }
+
+    public void setGred(String gred) {
+        this.gred = gred;
+    }
+
+    public String getBadanUsaha() {
+        return badanUsaha;
+    }
+
+    public void setBadanUsaha(String badanUsaha) {
+        this.badanUsaha = badanUsaha;
+    }
+
+    public String getPekerjaanUtama() {
+        return pekerjaanUtama;
+    }
+
+    public void setPekerjaanUtama(String pekerjaanUtama) {
+        this.pekerjaanUtama = pekerjaanUtama;
+    }
+
+    public String getBidangPekerjaanUtama() {
+        return bidangPekerjaanUtama;
+    }
+
+    public void setBidangPekerjaanUtama(String bidangPekerjaanUtama) {
+        this.bidangPekerjaanUtama = bidangPekerjaanUtama;
+    }
+
+    public String getTempatUsaha() {
+        return tempatUsaha;
+    }
+
+    public void setTempatUsaha(String tempatUsaha) {
+        this.tempatUsaha = tempatUsaha;
+    }
+
+    public int getBanyakPekerjaTetap() {
+        return banyakPekerjaTetap;
+    }
+
+    public void setBanyakPekerjaTetap(int banyakPekerjaTetap) {
+        this.banyakPekerjaTetap = banyakPekerjaTetap;
+    }
+
+    public ContactPerson getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(ContactPerson contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public Pencacah getPencacah() {
+        return pencacah;
+    }
+
+    public void setPencacah(Pencacah pencacah) {
+        this.pencacah = pencacah;
+    }
+
+    public Pengawas getPengawas() {
+        return pengawas;
+    }
+
+    public void setPengawas(Pengawas pengawas) {
+        this.pengawas = pengawas;
+    }
+
+    public String getCatatan() {
+        return catatan;
+    }
+
+    public void setCatatan(String catatan) {
+        this.catatan = catatan;
+    }
+
+    public Validator getValidasi() {
+        return validasi;
+    }
+
+    public void setValidasi(Validator validasi) {
+        this.validasi = validasi;
+    }
 }
+
--- src/kuisionerudpk/ValidatorPerson.java
+++ src/kuisionerudpk/ValidatorPerson.java
@@ -8,6 +8,55 @@ package kuisionerudpk;
  *
  * @author U53R
  */
-public class ValidatorPerson {
-    
+public class ValidatorPerson implements Validator {
+    private Person person; // Objek Person yang akan divalidasi
+
+    public ValidatorPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public boolean validate() throws ValidatorException {
+        // Lakukan validasi untuk semua atribut Person
+        // Validasi untuk nama
+        if (!isValidString(person.getNama())) {
+            throw new ValidatorException("Nama tidak boleh kosong");
+        }
+
+        // Validasi untuk jabatan
+        if (!isValidString(person.getJabatan())) {
+            throw new ValidatorException("Jabatan tidak boleh kosong");
+        }
+
+        // Jika semua validasi berhasil, kembalikan true
+        return true;
+    }
+
+    // Metode bantuan untuk memeriksa apakah string tidak kosong
+    private boolean isValidString(String str) {
+        return str != null && !str.trim().isEmpty();
+    }
+
+    @Override
+    public boolean save() {
+        System.out.println("Save here...");
+        return true;
+    }
+
+    @Override
+    public void print() {
+        // Mencetak informasi person
+        System.out.println("Nama: " + person.getNama());
+        System.out.println("Jabatan: " + person.getJabatan());
+        System.out.println("Tanggal: " + person.getTanggal());
+    }
+
+    @Override
+    public void reset() {
+        // Mereset semua atribut dalam objek Person
+        person.setNama("");
+        person.setJabatan("");
+        person.setTanggal(null);
+    }
 }
+
